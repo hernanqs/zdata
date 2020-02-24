042bc156fbe7f0848355f3f4a408ff8114d91a5a
hernanqs <hernan_quintero@live.com>
2020-02-24 18:36:15 -0300

    Los archivos CSV subidos se guardan en una base de datos SQLite

--- app.py
+++ app.py
@@ -1,5 +1,32 @@
 from flask import Flask, render_template, request
+import io
+import pandas as pd
 app = Flask(__name__)
+
+
+import sqlite3
+from flask import g
+
+DATABASE = './db/database.db'
+
+def get_db():
+	db = getattr(g, '_database', None)
+	if db is None:
+		db = g._database = sqlite3.connect(DATABASE)
+	db.row_factory = sqlite3.Row
+	return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+	db = getattr(g, '_database', None)
+	if db is not None:
+		db.close()
+
+def query_db(query, args=(), one=False):
+	cur = get_db().execute(query, args)
+	rv = cur.fetchall()
+	cur.close()
+	return (rv[0] if rv else None) if one else rv
 
 
 @app.route('/subir-archivo', methods=["GET", "POST"])
@@ -8,7 +35,11 @@ def subir_archivo():
 	if request.method == 'POST':
 		if request.files:
 			archivo = request.files['archivo']
-			print(archivo)
+			stream = io.StringIO(archivo.stream.read().decode("UTF8"), newline=None)
+			db = get_db()
+			header = 0 if 'con-nombre-de-columnas' in request.form else None
+			df = pd.read_csv(stream, header = header)
+			df.to_sql(request.form['nombre'], db, if_exists='replace', index=False)
 
 	return render_template("subir-archivo.html")
 
