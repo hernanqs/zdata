2633b7fc15004f1dba3dbd3abd8996a45d2afcf4
hernanqs <hernan_quintero@live.com>
2020-02-24 14:45:03 -0300

    Se hizo el formulario para subir archivos al servidor

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
+@app.route('/subir-archivo', methods=["GET", "POST"])
+def subir_archivo():
+
+	if request.method == 'POST':
+		if request.files:
+			archivo = request.files['archivo']
+			print(archivo)
+
+	return render_template("subir-archivo.html")
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
